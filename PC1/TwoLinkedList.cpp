ec3077007968cef748c9d8296b02d4d94cfdec1b
luiggiroal <luiggi.rodriguez@utec.edu.pe>
2019-04-12 13:11:59 -0500

    First

--- PC1/TwoLinkedList.cpp
+++ PC1/TwoLinkedList.cpp
@@ -3,3 +3,147 @@
 //
 
 #include "TwoLinkedList.h"
+
+TwoLinkedList::TwoLinkedList(){
+	head1 = nullptr;
+	head2 = nullptr;
+	tail1 = nullptr;
+	tail2 = nullptr;
+	tail = nullptr;
+}
+
+TwoLinkedList::~TwoLinkedList(){}
+
+void TwoLinkedList::push_back_1(int num){
+	Node* newNode; 
+	// Node* nodePtr;
+
+	newNode = new Node;
+	newNode->value = num;
+
+	newNode->next = nullptr;
+
+	if (!is_merge())
+	{
+		if (!head1)
+		{
+			head1 = newNode;
+			tail1 = newNode;
+		}else{
+			tail1 = head1;
+
+			while(tail1->next) {
+			    tail1= tail1->next;
+			}
+
+			tail1->next = newNode;
+			// tail1 = newNode;
+		}
+	}else{
+		tail->next = newNode;
+	}	
+}
+
+void TwoLinkedList::push_back_2(int num){
+	Node* newNode; 
+	// Node* nodePtr;
+
+	newNode = new Node;
+	newNode->value = num;
+
+	newNode->next = nullptr;
+
+	if (!is_merge())
+	{
+		if (!head2)
+		{
+			head2 = newNode;
+			tail2 = newNode;
+		}else{
+			tail2 = head2;
+
+			while(tail2->next) {
+			    tail2 = tail2->next;
+			}
+
+			tail2->next = newNode;
+			// tail2 = newNode;
+		}
+	}else{
+		tail->next = newNode;
+	}	
+}
+
+std::string TwoLinkedList::merge(int num){
+	Node* newNode;
+
+	newNode = new Node;
+	newNode->value = num;
+
+	newNode->next = nullptr;
+
+	if (!is_merge())
+	{
+		if (!head1)
+		{
+			head1 = newNode;
+			tail1 = newNode;
+		}else{
+			tail1 = head1;
+
+			while(tail1->next) {
+			    tail1= tail1->next;
+			}
+
+			tail1->next = newNode;
+			// tail1 = newNode;
+		}
+
+		if (!head2)
+		{
+			head2 = newNode;
+			tail2 = newNode;
+		}else{
+			tail2 = head2;
+
+			while(tail2->next) {
+			    tail2 = tail2->next;
+			}
+
+			tail2->next = newNode;
+			// tail2 = newNode;
+		}
+
+		return "Operación Exitosa";
+	}else{
+		delete newNode;
+
+		return "Operación duplicada";
+	}
+}
+
+bool TwoLinkedList::is_merge(){
+	if (!head1 && !head2)
+	{
+		return 0;	
+	}else{
+		if (tail1->next == tail2->next)
+		{
+				return 1;	
+		}else{
+			return 0;
+		}	
+	}
+}
+
+// std::string TwoLinkedList::getlist(int num){
+
+// }
+
+Node* TwoLinkedList::search(int num){
+	
+	if (!head)
+	{
+		
+	}
+}
--- PC1/TwoLinkedList.h
+++ PC1/TwoLinkedList.h
@@ -5,10 +5,36 @@
 #ifndef PC1_TWOLINKEDLIST_H
 #define PC1_TWOLINKEDLIST_H
 
+#include <iostream>
+#include <string>
 
 class TwoLinkedList {
+	struct Node
+	{
+		int value;
+		struct Node* next;
+	};
 
+	Node* head1;
+	Node* head2;
+	Node* tail1;
+	Node* tail2;
+	Node* tail;
+public:
+	TwoLinkedList();
+	~TwoLinkedList();
+
+	void push_back_1(int);
+	void push_back_2(int);
+	std::string merge(int);
+	bool is_merge();
+	std::string getlist(int);
+	Node* search(int);
+	auto save();
+	auto load();
 };
+
+
 
 
 #endif //PC1_TWOLINKEDLIST_H
